930bafff792e119200feeaba8a42fd8898dab984
yeonyeo <sylim0513@gmail.com>
2025-08-15 23:44:37 +0900

    feat:엔드포인트에 /api 프리픽스 추가

--- src/main/java/com/studymate/domain/user/controller/UserController.java
+++ src/main/java/com/studymate/domain/user/controller/UserController.java
@@ -17,7 +17,7 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 public class UserController {
 
     private final UserService userService;
